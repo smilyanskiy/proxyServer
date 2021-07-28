7726f9829b201376e5ae96c38d34a9822464f64e
Smilyanskiy <v.smilyanskiy@betinvest.com>
2021-07-28 14:24:48 +0300

    press F

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ const bodyParser = require("body-parser");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const api = require("./api");
 
-// Create Express Server
 const app = express();
 
-// Configuration
 const PORT = 4001;
 const HOST = "localhost";
 
 app.use(bodyParser.json());
 app.use("/", api);
 
+app.use("", (req, res, next) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 app.use(
   "/frontend_api2",
   createProxyMiddleware({
     target: process.env.API_SERVICE_URL,
     changeOrigin: true,
+    secure: false,
   })
 );
 
-// Start Proxy
 app.listen(PORT, HOST, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
